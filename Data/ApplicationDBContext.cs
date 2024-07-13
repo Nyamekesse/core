fc24fbf3b9e8c9f99e796df88fc8ebd990860e5a
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-13 23:58:34 +0000

    added Genre DbSet and also refactored primary constructor

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -1,19 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 
-public class ApplicationDBContext: DbContext
+public class ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : DbContext(options)
 {
 
     public DbSet<Book> Books { get; set; }
+
+    public DbSet<Genre> Genres { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.Entity<Book>().Property(p=>p.Price).HasPrecision(10,5);
-    }
-    
-    public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
-        : base(options)
-    {
     }
 }
