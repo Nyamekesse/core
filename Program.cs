e1e16bb4111692fef9eae4c4e1022db4303ecec5
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-13 20:37:32 +0000

    refactored code, moved database connection initialization above app builder

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+DotNetEnv.Env.Load();
+
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbUserName = Environment.GetEnvironmentVariable("DB_USERNAME");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<ApplicationDBContext>(options => { options.UseNpgsql($"Host={dbHost}; Database={dbName}; Username={dbUserName}; Password={dbPassword}"); });
+
 var app = builder.Build();
 
-DotNetEnv.Env.Load();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
