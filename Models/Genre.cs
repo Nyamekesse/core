71e1c6cfcc6d3f676d761ca5d6f9f6dc6002b613
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-14 00:49:54 +0000

    updated table and column name in Genre table

--- Models/Genre.cs
+++ Models/Genre.cs
@@ -1,6 +1,11 @@
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("tb_genres")]
 public class Genre
 {
     public int Id { get; set; }
+    [Column("Name")]
     public string GenreName { get; set; }
 
     public int DisplayOrder { get; set; }
