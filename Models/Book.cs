bc7420e43f2b73ef07f06a9ff0a184b18ef047d0
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-14 01:26:00 +0000

    added a not mapped column to Book table

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 public class Book
 {
     public int Id { get; set; }
     public string Title { get; set; }
     public string ISBN { get; set; }
     public decimal Price { get; set; }
+
+    [NotMapped]
+    public string PriceRange { get; set; }
 }
