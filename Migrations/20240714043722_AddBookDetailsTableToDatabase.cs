2a0a60bc2087609b19d3f30f9628649027cc8330
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-14 04:48:59 +0000

    added new model book details, updated database via migration

---
+++ Migrations/20240714043722_AddBookDetailsTableToDatabase.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ef_core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookDetailsTableToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BookDetails",
+                columns: table => new
+                {
+                    BookDEtail_I = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    NumberOfChapters = table.Column<int>(type: "integer", nullable: false),
+                    NumberOfPages = table.Column<int>(type: "integer", nullable: false),
+                    Weight = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookDetails", x => x.BookDEtail_I);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookDetails");
+        }
+    }
+}
