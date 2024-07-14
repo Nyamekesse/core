8335b03adf88d49119e6682c162bce7ba504faf9
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-14 15:40:06 +0000

    manually created a class to describe many to many reationship between Books and Authors instead of using EF

---
+++ Models/BookAuthorMap.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ef_core.Models
+{
+    public class BookAuthorMap
+    {
+        [ForeignKey("Book")]
+        public int Book_Id { get; set; }
+
+        [ForeignKey("Author")]
+        public int Author_Id { get; set; }
+        public Book Book { get; set; }
+        public Author Author { get; set; }
+    }
+}
