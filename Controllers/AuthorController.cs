26226860aa9e7e7947e57ba5b5ae883b116fc350
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-17 17:23:45 +0000

    added controllers endpoints for Publishers and Authors

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ef_core;
+
+public class AuthorController(ApplicationDBContext _db) : Controller
+{
+    public IActionResult Index()
+    {
+        List<Author> objList = _db.Authors.ToList();
+        return View(objList);
+    }
+
+    public IActionResult Upsert(int? id)
+    {
+        Author obj = new();
+        if (id is null || id == 0)
+        {
+            //create
+            return View(obj);
+        }
+        else
+        {
+            //update
+            obj = _db.Authors.FirstOrDefault(c => c.Author_Id == id);
+            if (obj is null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Upsert(Author obj)
+    {
+        if (ModelState.IsValid)
+        {
+            if (obj.Author_Id == 0)
+            {
+                await _db.Authors.AddAsync(obj);
+            }
+            else
+            {
+                _db.Authors.Update(obj);
+            }
+
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(obj);
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        Author obj = new();
+
+        obj = _db.Authors.FirstOrDefault(c => c.Author_Id == id);
+        if (obj is null)
+        {
+            return NotFound();
+        }
+        _db.Authors.Remove(obj);
+        await _db.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ Controllers/PublisherController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ef_core;
+
+public class PublisherController(ApplicationDBContext _db) : Controller
+{
+    public IActionResult Index()
+    {
+        List<Publisher> objList = [.. _db.Publishers];
+        return View(objList);
+    }
+
+    public IActionResult Upsert(int? id)
+    {
+        Publisher obj = new();
+        if (id is null || id == 0)
+        {
+            //create
+            return View(obj);
+        }
+        else
+        {
+            //update
+            obj = _db.Publishers.FirstOrDefault(c => c.Publisher_Id == id);
+            if (obj is null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Upsert(Publisher obj)
+    {
+        if (ModelState.IsValid)
+        {
+            if (obj.Publisher_Id == 0)
+            {
+                await _db.Publishers.AddAsync(obj);
+            }
+            else
+            {
+                _db.Publishers.Update(obj);
+            }
+
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(obj);
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        Publisher obj = new();
+
+        obj = _db.Publishers.FirstOrDefault(c => c.Publisher_Id == id);
+        if (obj is null)
+        {
+            return NotFound();
+        }
+        _db.Publishers.Remove(obj);
+        await _db.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+}
