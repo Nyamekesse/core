1d1ce57594a65a19cf4f1e45e7fe52d2f77db8b4
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-18 12:58:09 +0000

    added controllers for Books

---
+++ Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using ef_core.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ef_core;
+
+public class BookController(ApplicationDBContext _db) : Controller
+{
+    public IActionResult Index()
+    {
+        List<Book> objList = [.. _db.Books];
+        return View(objList);
+    }
+
+    public IActionResult Upsert(int? id)
+    {
+        BookViewModel obj = new();
+
+        obj.PublisherList = _db.Publishers.Select(p => new SelectListItem
+        {
+            Text = p.Name,
+            Value = p.Publisher_Id.ToString()
+        });
+
+        if (id is null || id == 0)
+        {
+            return View(obj);
+        }
+        obj.Book = _db.Books.FirstOrDefault(b => b.Id == id);
+        if (obj is null)
+        {
+            return NotFound();
+        }
+        return View(obj);
+    }
+}
