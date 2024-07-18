9ae8735e958a1f9b06df76ec2378f694932a55fd
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-18 12:56:30 +0000

    created separate view model to hold selected data

---
+++ ViewModels/BookViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ef_core.ViewModels;
+
+public class BookViewModel
+{
+    public Book Book { get; set; }
+    public IEnumerable<SelectListItem> PublisherList { get; set; }
+}
